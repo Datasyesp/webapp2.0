6ae202113d0fa10858f837dd0853f8e0c282bf52
Datasyesp <datasyesp@gmail.com>
2025-07-06 23:57:59 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function HomePage() {
       <About />
       <Services />
       <Products />
-      <TrustedClients />
+{/*       <TrustedClients /> */}
       <Clients />
       <Contact />
       <Footer />
